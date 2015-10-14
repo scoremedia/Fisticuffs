d247b627dcb0ed2f96161550375178495da01086
Darren Clark <darren.clark@thescore.com>
2015-10-14 13:30:36 -0400

    Add (failing) DependencyTracker tests

---
+++ Example/Tests/DependencyTrackerSpec.swift
@@ -0,0 +1,45 @@
+//
+//  DependencyTrackerSpec.swift
+//  Bindings
+//
+//  Created by Darren Clark on 2015-10-13.
+//  Copyright © 2015 Darren Clark. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+@testable import Bindings
+
+
+class DependencyTrackerSpec: QuickSpec {
+    override func spec() {
+        
+        it("should collect all Observable's accessed inside the passed in block") {
+            let hello = Observable("Hello")
+            let world = Observable("world")
+            
+            let dependencies = DependencyTracker.findDependencies {
+                let _ = "\(hello.value), \(world.value)"
+            }
+            
+            expect(dependencies.count) == 2
+            expect(dependencies.contains { $0 === hello }) == true
+            expect(dependencies.contains { $0 === world }) == true
+        }
+        
+        it("should not return duplicate usages of the same Observable") {
+            let test = Observable("test")
+            
+            let dependencies = DependencyTracker.findDependencies {
+                let _ = "\(test.value)ing"
+                let _ = "\(test.value)er"
+                let _ = "\(test.value)ed"
+            }
+            
+            expect(dependencies.count) == 1
+            expect(dependencies.contains { $0 === test }) == true
+        }
+        
+    }
+}
