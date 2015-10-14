3893fdc5a6d377b11f2594d367753298b6298c44
Darren Clark <darren.clark@thescore.com>
2015-10-14 16:35:59 -0400

    Refactor away BindingState

---
+++ Pod/Classes/UIKit/NSObject+AssociatedObjects.swift
@@ -0,0 +1,37 @@
+//
+//  NSObject+AssociatedObjects.swift
+//  Pods
+//
+//  Created by Darren Clark on 2015-10-14.
+//
+//
+
+import Foundation
+
+private var keys = [String:NSObject]()
+
+extension NSObject {
+    
+    func findKey(name: String, type: Any) -> UnsafePointer<Void> {
+        let keyString = "\(name)__\(type)"
+        if let keyObj = keys[keyString] {
+            return UnsafePointer(Unmanaged.passUnretained(keyObj).toOpaque())
+        }
+        else {
+            let keyObj = NSObject()
+            keys[keyString] = keyObj
+            return UnsafePointer(Unmanaged.passUnretained(keyObj).toOpaque())
+        }
+    }
+    
+    func get<T: AnyObject>(name: String) -> T? {
+        let key = findKey(name, type: T.self)
+        return objc_getAssociatedObject(self, key) as? T
+    }
+    
+    func set<T: AnyObject>(name: String, value: T?) {
+        let key = findKey(name, type: T.self)
+        objc_setAssociatedObject(self, key, value, .OBJC_ASSOCIATION_RETAIN)
+    }
+    
+}
--- Pod/Classes/UIKit/NSObject+Binding.swift
+++ Pod/Classes/UIKit/NSObject+Binding.swift
@@ -9,53 +9,18 @@
 import Foundation
 
 
-private var bindingStateKey = 0
-
-private class BindingState {
-    private var activeDisposables = [String: Disposable]()
-    private var activeObservables = [String: AnyObject]()  // String: Observer<T>
-    
-    deinit {
-        for (_, disposable) in activeDisposables {
-            disposable.dispose()
-        }
-    }
-}
-
 extension NSObject {
     
-    private var bindingState: BindingState {
-        if let existing = objc_getAssociatedObject(self, &bindingStateKey) as? BindingState {
-            return existing
-        }
-        else {
-            let value = BindingState()
-            objc_setAssociatedObject(self, &bindingStateKey, value, .OBJC_ASSOCIATION_RETAIN)
-            return value
-        }
-    }
-    
     func getObservableFor<T>(key: String) -> Observable<T>? {
-        if let observable = bindingState.activeObservables[key] as? Observable<T> {
-            return observable
-        }
-        else {
-            return nil
-        }
+        return get(key)
     }
     
     func setObservableFor<T>(key: String, observable: Observable<T>?, setter: (value: T) -> Void) {
-        let bindingState = self.bindingState
+        set(key, value: observable)
         
-        bindingState.activeObservables.removeValueForKey(key)
-        if let old = bindingState.activeDisposables.removeValueForKey(key) {
-            old.dispose()
-        }
-        
-        if let observable = observable {
-            bindingState.activeObservables[key] = observable
-            bindingState.activeDisposables[key] = observable.subscribe(setter)
-        }
+        let bag = DisposableBag()
+        observable?.subscribe(setter).addTo(bag)
+        set(key, value: bag)
     }
     
 }
