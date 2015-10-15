e766549880fd27294a68737e0493d3db9054820c
Darren Clark <darren.clark@thescore.com>
2015-10-15 15:05:53 -0400

    Table view animates its row changes now

--- Pod/Classes/UIKit/UITableView+Binding.swift
+++ Pod/Classes/UIKit/UITableView+Binding.swift
@@ -14,16 +14,11 @@ public extension UITableView {
         let config = TableViewConfig<T>()
         block(config)
         
-        let delegate = TableViewDelegate(items: items, config: config)
+        let delegate = TableViewDelegate(items: items, tableView: self, config: config)
         set("delegate", value: (delegate as AnyObject))
         
         self.delegate = delegate
         dataSource = delegate
-        
-        let subscription = items.subscribeArray { [weak self] _, _ in
-            self?.reloadData()
-        }
-        set("delegateSubscription", value: subscription)
     }
     
     
@@ -63,12 +58,61 @@ public class TableViewConfig<T> : NSObject {
 
 
 private class TableViewDelegate<T> : NSObject, UITableViewDataSource, UITableViewDelegate {
+    weak var tableView: UITableView?
     var items: ObservableArray<T>
     let config: TableViewConfig<T>
     
-    init(items: ObservableArray<T>, config: TableViewConfig<T>) {
+    let disposeBag = DisposableBag()
+    
+    
+    init(items: ObservableArray<T>, tableView: UITableView, config: TableViewConfig<T>) {
         self.items = items
+        self.tableView = tableView
         self.config = config
+        
+        super.init()
+        
+        items.subscribeArray { [weak self] _, change in
+            self?.applyChange(change)
+        }
+        .addTo(disposeBag)
+    }
+    
+    private func applyChange(change: ArrayChange<T>) {
+        switch change {
+        case .Initial:
+            tableView?.reloadData()
+            
+        case let .Insert(index, newElements):
+            let indexPaths = (index ..< index + newElements.count).map { row in NSIndexPath(forRow: row, inSection: 0) }
+            
+            let isAppending = index == items.count - newElements.count
+            let animation = isAppending ? UITableViewRowAnimation.None : .Top
+            tableView?.insertRowsAtIndexPaths(indexPaths, withRowAnimation: animation)
+            
+        case let .Remove(range, _):
+            let indexPaths = range.map { row in NSIndexPath(forRow: row, inSection: 0) }
+            let animation = tableView?.editing == true ? UITableViewRowAnimation.Right : .Top
+            tableView?.deleteRowsAtIndexPaths(indexPaths, withRowAnimation: animation)
+            
+        case let .Replace(range, removedElements, newElements):
+            if range.count == removedElements.count {
+                // replaced all items, just reload
+                tableView?.reloadData()
+            }
+            else {
+                tableView?.beginUpdates()
+                
+                let deleted = range.map { row in NSIndexPath(forRow: row, inSection: 0) }
+                tableView?.deleteRowsAtIndexPaths(deleted, withRowAnimation: .Top)
+                
+                let addedIndexes = range.startIndex ..< range.startIndex + newElements.count
+                let added = addedIndexes.map { row in NSIndexPath(forRow: row, inSection: 0) }
+                tableView?.insertRowsAtIndexPaths(added, withRowAnimation: .Top)
+                
+                tableView?.endUpdates()
+            }
+        }
     }
     
     @objc func numberOfSectionsInTableView(tableView: UITableView) -> Int {
