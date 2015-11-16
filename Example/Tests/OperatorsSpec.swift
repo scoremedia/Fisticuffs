cce97c3aa18218de634205ef2864464cd9aeb465
Darren Clark <darren.clark@thescore.com>
2015-11-16 16:57:18 -0500

    Add some operator (ab)use

---
+++ Example/Tests/OperatorsSpec.swift
@@ -0,0 +1,106 @@
+//
+//  OperatorsSpec.swift
+//  SwiftMVVMBinding
+//
+//  Created by Darren Clark on 2015-11-16.
+//  Copyright © 2015 Darren Clark. All rights reserved.
+//
+
+import Foundation
+import Quick
+import Nimble
+@testable import SwiftMVVMBinding
+
+
+class OperatorsSpec: QuickSpec {
+    override func spec() {
+        var observable: Observable<Int>!
+        var anySubscribable: AnySubscribable!
+        
+        var underlyingValue: Int!
+        var binding: Binding<Int>!
+        var bidirectionalBinding: BidirectionalBinding<Int>!
+        
+        beforeEach {
+            observable = Observable(11)
+            anySubscribable = observable
+            
+            underlyingValue = 0
+            binding = Binding<Int>(setter: { value in underlyingValue = value })
+            bidirectionalBinding = BidirectionalBinding<Int>(getter: { underlyingValue }, setter: { underlyingValue = $0 })
+        }
+        
+        describe("+=") {
+            it("should be used for subscribing to Subscribables") {
+                var receivedValue = 0
+                observable += { _, new in receivedValue = new }
+                expect(receivedValue) == 11
+            }
+            
+            it("should be used for subscribing to AnySubscribables") {
+                var receivedEvent = false
+                anySubscribable += { receivedEvent = true }
+                expect(receivedEvent) == true
+            }
+        }
+        
+        describe("<--") {
+            it("should support binding Bindings to Subscribables") {
+                binding <-- observable
+                expect(underlyingValue) == 11
+            }
+            
+            it("should supprt binding BidirectionalBindings to Observables") {
+                bidirectionalBinding <-- observable
+                expect(underlyingValue) == 11
+            }
+            
+            it("should NOT setup a two-way binding between BidirectionalBindings and Observables") {
+                bidirectionalBinding <-- observable
+                expect(underlyingValue) == 11
+                
+                underlyingValue = 5
+                bidirectionalBinding.pushChangeToObservable()
+                
+                // since its a one way binding, should still be 11
+                expect(observable.value) == 11
+            }
+        }
+        
+        describe("-->") {
+            it("should support binding Bindings to Subscribables") {
+                observable --> binding
+                expect(underlyingValue) == 11
+            }
+            
+            it("should supprt binding BidirectionalBindings to Observables") {
+                observable --> bidirectionalBinding
+                expect(underlyingValue) == 11
+            }
+            
+            it("should NOT setup a two-way binding between BidirectionalBindings and Observables") {
+                observable --> bidirectionalBinding
+                expect(underlyingValue) == 11
+                
+                underlyingValue = 5
+                bidirectionalBinding.pushChangeToObservable()
+                
+                // since its a one way binding, should still be 11
+                expect(observable.value) == 11
+            }
+        }
+        
+        describe("<->") {
+            it("should support two-way binding between BidirectionalBindings and Observables") {
+                bidirectionalBinding <-> observable
+                expect(underlyingValue) == 11
+                
+                underlyingValue = 5
+                bidirectionalBinding.pushChangeToObservable()
+                
+                // since its a one way binding, should still be 11
+                expect(observable.value) == 5
+            }
+        }
+    }
+}
---
+++ Pod/Classes/Operators.swift
@@ -0,0 +1,52 @@
+//
+//  Operators.swift
+//  Pods
+//
+//  Created by Darren Clark on 2015-11-16.
+//
+//
+
+//MARK: - Operators
+
+infix operator <-- {}
+infix operator --> {}
+infix operator <-> {}
+
+
+//MARK: - One way binding
+
+public func <--<T, S where S: Subscribable, S.ValueType == T>(lhs: Binding<T>, rhs: S) {
+    lhs.bind(rhs)
+}
+
+public func --><T, S where S: Subscribable, S.ValueType == T>(lhs: S, rhs: Binding<T>) {
+    rhs.bind(lhs)
+}
+
+public func <--<T, S where S: Subscribable, S.ValueType == T>(lhs: BidirectionalBinding<T>, rhs: S) {
+    lhs.bind(rhs)
+}
+
+public func --><T, S where S: Subscribable, S.ValueType == T>(lhs: S, rhs: BidirectionalBinding<T>) {
+    rhs.bind(lhs)
+}
+
+//MARK: - Two way binding
+
+public func <-><T>(lhs: BidirectionalBinding<T>, rhs: Observable<T>) {
+    lhs.bind(rhs)
+}
+
+public func <-><T>(lhs: Observable<T>, rhs: BidirectionalBinding<T>) {
+    rhs.bind(lhs)
+}
+
+//MARK: - Subscriptions
+
+public func +=<T, S where S: Subscribable, S.ValueType == T>(lhs: S, rhs: (T?, T) -> Void) {
+    lhs.subscribe(rhs)
+}
+
+public func +=(lhs: AnySubscribable, rhs: () -> Void) {
+    lhs.subscribe(rhs)
+}
