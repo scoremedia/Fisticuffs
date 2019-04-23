f23cc61fd9259af9ed88ad2a600ace3fcefe2953
Darren Clark <darren.clark@thescore.com>
2019-04-22 22:21:06 -0400

    hacks to get compiling for macos (breaks ios)
    experimenting with https://ci.swift.org/job/swift-PR-toolchain-osx/287//artifact/branch-master/swift-PR-23701-287-osx.tar.gz

--- Source/Fisticuffs.h
+++ Source/Fisticuffs.h
@@ -6,7 +6,7 @@
 //  Copyright © 2015 theScore. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Fisticuffs.
 FOUNDATION_EXPORT double FisticuffsVersionNumber;
